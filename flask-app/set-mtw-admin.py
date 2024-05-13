29c831206486be016cdf1ea23b9944558a33070e
filak <filak@users.noreply.github.com>
2024-05-13 17:46:47 +0200

    Cleaned using Flake8

--- flask-app/set-mtw-admin.py
+++ flask-app/set-mtw-admin.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-import os, argparse, configparser
+import os
+import argparse
+import configparser
 import bcrypt
 from pathlib import Path
 
 appname = 'set-mtw-admin'
 appversion = '1.3.1'
 appdesc = '(re)Set Admin credentials and SecKeys in MTW Admin Config'
-appusage = 'Help:   '+ appname +' -h '
+appusage = 'Help:   ' + appname + ' -h '
+
 
 def main():
 
@@ -18,7 +21,9 @@ def main():
     print('*  ', appusage)
     print('********************************************\n')
 
-    parser = argparse.ArgumentParser(description=appdesc, prog=appname, usage='%(prog)s --login ... -pwd ... ')
+    parser = argparse.ArgumentParser(description=appdesc,
+                                     prog=appname,
+                                     usage='%(prog)s --login ... -pwd ... ')
     parser.add_argument('--login', type=str, help='Admin login')
     parser.add_argument('--pwd', type=str, help='Admin password')
     parser.add_argument('--worker', help='Reset Worker section only', action='store_true')
@@ -27,7 +32,7 @@ def main():
 
     if unknown:
         print('ERROR : Uknown arguments : ', unknown)
-        print('Try : ', appname ,' -h')
+        print('Try : ', appname, ' -h')
 
     elif args.worker or (args.login and args.pwd):
         procFile(args)
@@ -42,22 +47,22 @@ def procFile(args):
     config = configparser.RawConfigParser()
 
     if args.login and args.pwd and not args.worker:
-    
+
         section = 'adminconf'
         config.add_section(section)
-        config.set(section, 'SECRET_KEY', str(os.urandom(24) ).replace('%','%%') )
+        config.set(section, 'SECRET_KEY', str(os.urandom(24)).replace('%', '%%'))
         config.set(section, 'ADMINNAME', args.login)
 
         pwd = args.pwd
         pwd_hashed = bcrypt.hashpw(pwd.encode('utf-8'), bcrypt.gensalt(5))
-        config.set(section, 'ADMINPASS', pwd_hashed.decode('utf-8') )
+        config.set(section, 'ADMINPASS', pwd_hashed.decode('utf-8'))
 
         section = 'worker'
         config.add_section(section)
-        config.set(section, 'API_KEY', str(os.urandom(24) ).replace('%','%%') )
+        config.set(section, 'API_KEY', str(os.urandom(24)).replace('%', '%%'))
 
         with open(str(configFile), 'w', encoding='utf-8') as configfile:
-            config.write(configfile)        
+            config.write(configfile)
 
     elif args.worker and configFile.exists():
 
@@ -66,11 +71,10 @@ def procFile(args):
         section = 'worker'
         config.remove_section(section)
         config.add_section(section)
-        config.set(section, 'API_KEY', str(os.urandom(24) ).replace('%','%%') )
+        config.set(section, 'API_KEY', str(os.urandom(24)).replace('%', '%%'))
 
         with open(str(configFile), 'w', encoding='utf-8') as configfile:
             config.write(configfile)
-
 
     if configFile.exists():
         print('\nAdmin Config updated: ', str(configFile), '\n')
@@ -82,4 +86,3 @@ def procFile(args):
 
 if __name__ == '__main__':
     main()
-

fd6d65452123fe672a38a3c7ebd8d0f5fc1a6581
filak <filak@users.noreply.github.com>
2024-10-17 12:59:42 +0200

    Version 1.6.7

--- flask-app/run-worker-dev.py
+++ flask-app/run-worker-dev.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
+from application.worker import create_app
+
 DEFAULT_PORT = 5903
 DEFAULT_CONFIG = 'conf/mtw.ini'
 
-import sys
-from application.worker import create_app
 
 def start():
     if len(sys.argv) == 2:
@@ -17,4 +18,3 @@ def start():
 
 if __name__ == "__main__":
     start()
-
